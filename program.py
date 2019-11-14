75842c5b403c61e4e4734e9bfeb444b535b92456
imansedgh <iman.sedgh@yahoo.com>
2019-11-14 23:21:58 +0330

    adding github Address

--- program.py
+++ program.py
@@ -3,7 +3,7 @@ from telegram.ext import Updater, CommandHandler
 updater = Updater('931989944:AAGpaQSgW3KQHKnGll2gJKdGKXiywVPonNI', use_context=True)
 quality = python.airquality()
 def start_method(bot, update):
-    bot.message.reply_text("Welcome to Our Bot. Developer :  @Iman_Sedgh  ")
+    bot.message.reply_text("Welcome to Our Bot.\n Developer :  @Iman_Sedgh  \n This is a Free Python Script Using BS4 and Python-telegram-bot modules \n Github : https://github.com/iman-sedgh/Air-pollution-Scraping ")
     bot.message.reply_text("To Get Air Quality Use /Get")
     print ('new message')
 
