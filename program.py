b8e27f5b4084ed3ab75da6ec820020030632317a
imansedgh <iman.sedgh@yahoo.com>
2019-11-29 19:15:52 +0330

    fixing bugs

--- program.py
+++ program.py
@@ -2,7 +2,7 @@ import python
 from telegram.ext import Updater, CommandHandler
 token = '931989944:AAGpaQSgW3KQHKnGll2gJKdGKXiywVPonNI'
 updater = Updater(token, use_context=True)
-quality = python.airquality(
+quality = python.airquality()
 def start_method(bot, update):
     bot.message.reply_text("Welcome to Our Bot.\n Developer :  @Iman_Sedgh  \n This is a Free Python Script Using BS4 and Python-telegram-bot modules \n Github : https://github.com/iman-sedgh/Air-pollution-Scraping ")
     bot.message.reply_text("To Get Air Quality Use /Get")
