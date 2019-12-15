fb9c0eafd2bc5dd28e4e31e820394ae43c7407d1
imansedgh <iman.sedgh@yahoo.com>
2019-12-15 19:03:39 +0330

    adding inline mode (beta), adding multithreading feature , bug fixed (run.py)

--- run.py
+++ run.py
@@ -1,24 +1,57 @@
 import scraper
-from telegram.ext import Updater, CommandHandler
-token = '931989944:AAGpaQSgW3KQHKnGll2gJKdGKXiywVPonNI'
+import logging
+from uuid import uuid4
+from telegram.ext import Updater
+from telegram.ext import CommandHandler
+from telegram.ext import InlineQueryHandler
+from telegram import InputTextMessageContent
+from telegram import InlineQueryResultArticle
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+#token = '931989944:AAGpaQSgW3KQHKnGll2gJKdGKXiywVPonNI'
+token = '422767920:AAEZVBVBYoCrWu4Pula_eccUiYDcn5FPCq0'
+
 updater = Updater(token, use_context=True)
 quality = scraper.airquality()
 def start_method(bot, update):
     bot.message.reply_text("Welcome to Our Bot.\n Developer :  @Iman_Sedgh  \n This is a Free Python Script Using BS4 and Python-telegram-bot modules \n Github : https://github.com/iman-sedgh/Air-pollution-Scraping ")
     bot.message.reply_text("To Get Air Quality Index Use /Get")
     print ('new message')
-
 def get_method(bot,update):
     print('get method')
     bot.message.reply_text("Getting Index From {}".format(quality.url))
     index = quality.getairquality()
     print(index)
     bot.message.reply_text("شاخص آلودگی هوای شهر تهران در حال حاضر {}".format(index))
-start_command = CommandHandler('start', start_method)
-updater.dispatcher.add_handler(start_command)
-updater.dispatcher.add_handler(CommandHandler('Get',get_method))
 
-updater.start_polling()
+def inline_feature(bot,update):
+    query = bot.inline_query.query
+    results = []
+    results.append(InlineQueryResultArticle(id = uuid4(),title="English",input_message_content = InputTextMessageContent("Air Quality Index Of Tehran Is {}".format("English"+str(query)))))
+    results.append(InlineQueryResultArticle(id = uuid4(),title="Persian",input_message_content = InputTextMessageContent("شاخص آلودگی هوای تهران در حال حاظر {}".format("persian"+str(query)))))
+    bot.inline_query.answer(results)
+
+
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+
+start_command = CommandHandler('start', start_method)
+get_command = CommandHandler('get',get_method)
+index_inline = InlineQueryHandler(inline_feature)
+updater.dispatcher.add_handler(start_command)
+updater.dispatcher.add_handler(get_command)
+updater.dispatcher.add_handler(index_inline)
+updater.dispatcher.add_error_handler(error)
+import threading
+t1 = threading.Thread(target=updater.start_polling, args = () )
+t2 = threading.Thread(target=quality.threadfunc, args = () )
+t1.start()
+t2.start()
+#updater.start_polling()
 
 
 updater.idle()
