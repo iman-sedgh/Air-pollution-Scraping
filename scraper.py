965452cc3670c90001faeed4a46db851bd0cd7fd
imansedgh <iman.sedgh@yahoo.com>
2019-12-15 19:03:56 +0330

    adding threading and etc

--- scraper.py
+++ scraper.py
@@ -1,17 +1,20 @@
 class airquality:
-
+    data = []
     def __init__(self,dataurl="https://airnow.tehran.ir/"):
         self.url= dataurl
                 
-    
+    def threadfunc(self):
+        while (1):
+            import time
+            from requests import get
+            from bs4 import BeautifulSoup
+            response = get(self.url)
+            html_soup = BeautifulSoup(response.text , 'html.parser')
+            tmpdata = html_soup.find('span',attrs={"id":"ContentPlaceHolder1_lblAqi3h"})
+            self.data.append(tmpdata.text)
+            print('exiting thread func')
+            time.sleep(60)
+
     def getairquality(self):
-        from requests import get
-        from bs4 import BeautifulSoup
-        response = get(self.url)
-        #print ("geting URL")
-     
-        html_soup = BeautifulSoup(response.text , 'html.parser')
-        data = html_soup.find('span',attrs={"id":"ContentPlaceHolder1_lblAqi3h"})
-        data = data.text
-        return data 
-#print airquality.getairquality()
+        return self.data[-1]
+        
