7989f9ab7c3ecbf5eea33eccb9fcf86b935ccd1d
imansedgh <iman.sedgh@yahoo.com>
2020-01-07 14:50:41 +0330

    changing source to airvisual

--- scraper.py
+++ scraper.py
@@ -1,6 +1,6 @@
 class airquality:
     data = []
-    def __init__(self,dataurl="http://airnow.tehran.ir/"):
+    def __init__(self,dataurl="https://www.airvisual.com/iran/tehran"):
         self.url= dataurl
                 
     def threadfunc(self):
@@ -10,16 +10,17 @@ class airquality:
             import requests
             from bs4 import BeautifulSoup
             try:
-                response = get(self.url, timeout=60)
+                response = get(self.url, timeout=10)
             except requests.exceptions.Timeout as e:
                 print ('excepted')
             
             html_soup = BeautifulSoup(response.text , 'html.parser')
-            tmpdata = html_soup.find('span',attrs={"id":"ContentPlaceHolder1_lblAqi3h"})
-            self.data.append(tmpdata.text)
+            tmpdata = html_soup.find('span',attrs={'aqi'})
+            aqi = tmpdata.text
+            aqi = aqi[0:-6]
+            self.data.append(aqi)
             print('exiting thread func')
             time.sleep(300)
 
     def getairquality(self):
         return self.data[-1]
-        
